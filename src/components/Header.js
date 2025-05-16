779fd34ae946782a2ea7d178adffae63865d7488
Varshhhininandhu <153708072+Varshhhininandhu@users.noreply.github.com>
2025-05-16 23:31:24 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,19 +4,16 @@ import { Link } from 'react-scroll';
 const Header = () => {
   return (
     <header className="fixed top-0 left-0 w-full z-50 bg-gradient-to-r from-black via-[#2e026d]/70 to-[#0f172a]/80 shadow-md">
-      <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
-        
-        {/* Logo Image */}
+      <div className="max-w-7xl mx-auto px-6 py-2 flex justify-between items-center">
         <div className="flex items-center gap-2">
           <img
             src="/logome.jpeg"
             alt="Logo"
-            className="w-16 h-16 object-contain"
+            className="w-12 h-12 object-contain"
           />
         </div>
 
-        {/* Nav Links */}
-        <nav className="space-x-6 text-white font-semibold">
+        <nav className="space-x-4 text-white font-semibold text-sm">
           <Link to="home" smooth={true} duration={500} className="cursor-pointer hover:text-purple-400">
             Home
           </Link>
